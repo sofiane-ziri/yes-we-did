1b6ac646487c7e81600ad5b5d43c1f5b5c1de978
sofiane-ziri <91732738+sofiane-ziri@users.noreply.github.com>
2022-07-19 17:11:44 +0200

    restart

--- admin/model/connect-bdd.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-try {
-    $bdd = new PDO("mysql:host=localhost;dbname=moduleconnexion;charset=utf8", "root", "root");
-} catch (PDOException $e) {
-    die('Erreur : ' . $e->getMessage());
-}
